982326dcf6eebdaf1599988f18060ae6695c23cd
ryotohiji <ryotohiji.41@gmail.com>
2026-02-03 08:06:57 +0900

    ヘッダー部分一通り実装完了

--- front-page.php
+++ front-page.php
@@ -10,16 +10,22 @@ get_header();
                     <span class="p-top-mv__title-main">ご家族の近くで</span>
                     <span class="p-top-mv__title-main">安心の医療を</span>
                 </h1>
-                <p class="p-top-mv__text">地域に根ざした医療で、<br class="u-sp-only">皆さまの健康をサポートします。</p>
+                <p class="p-top-mv__text">交通の利便のよい大阪市内。<br>
+                    地域で暮らすご家族が通いやすい立地で、外来診療のかかりつけとして72床の入院環境も含め、<br>
+                    きめ細やかな医療とケアを提供しています。</p>
                 <div class="p-top-mv__buttons">
-                    <a href="<?php echo esc_url(home_url('/contact')); ?>" class="c-btn c-btn--primary c-btn--lg">予約・お問い合わせ</a>
-                    <a href="<?php echo esc_url(home_url('/about')); ?>" class="c-btn c-btn--outline c-btn--lg">詳細はこちら</a>
+                    <a href="<?php echo esc_url(home_url('/contact')); ?>"
+                        class="c-btn c-btn--primary c-btn--lg">予約・お問い合わせ</a>
+                    <a href="<?php echo esc_url(home_url('/about')); ?>"
+                        class="c-btn c-btn--outline c-btn--lg">詳細はこちら</a>
                 </div>
             </div>
         </div>
         <div class="p-top-mv__wave">
             <svg viewBox="0 0 1440 120" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
-                <path d="M0 120L60 105C120 90 240 60 360 45C480 30 600 30 720 37.5C840 45 960 60 1080 67.5C1200 75 1320 75 1380 75L1440 75V120H1380C1320 120 1200 120 1080 120C960 120 840 120 720 120C600 120 480 120 360 120C240 120 120 120 60 120H0Z" fill="#FFF9F5"/>
+                <path
+                    d="M0 120L60 105C120 90 240 60 360 45C480 30 600 30 720 37.5C840 45 960 60 1080 67.5C1200 75 1320 75 1380 75L1440 75V120H1380C1320 120 1200 120 1080 120C960 120 840 120 720 120C600 120 480 120 360 120C240 120 120 120 60 120H0Z"
+                    fill="#FFF9F5" />
             </svg>
         </div>
     </section>
@@ -36,33 +42,36 @@ get_header();
                     'post_status' => 'publish'
                 ));
 
-                if ($news_query->have_posts()) :
-                    while ($news_query->have_posts()) : $news_query->the_post();
-                ?>
-                <li class="p-top-news__item">
-                    <a href="<?php the_permalink(); ?>" class="p-top-news__link">
-                        <time class="p-top-news__date" datetime="<?php echo esc_attr(get_the_date('Y-m-d')); ?>"><?php echo esc_html(get_the_date('Y.m.d')); ?></time>
-                        <?php
-                        $categories = get_the_category();
-                        if ($categories) :
+                if ($news_query->have_posts()):
+                    while ($news_query->have_posts()):
+                        $news_query->the_post();
                         ?>
-                        <span class="p-top-news__category"><?php echo esc_html($categories[0]->name); ?></span>
-                        <?php endif; ?>
-                        <span class="p-top-news__item-title"><?php the_title(); ?></span>
-                    </a>
-                </li>
-                <?php
+                        <li class="p-top-news__item">
+                            <a href="<?php the_permalink(); ?>" class="p-top-news__link">
+                                <time class="p-top-news__date"
+                                    datetime="<?php echo esc_attr(get_the_date('Y-m-d')); ?>"><?php echo esc_html(get_the_date('Y.m.d')); ?></time>
+                                <?php
+                                $categories = get_the_category();
+                                if ($categories):
+                                    ?>
+                                    <span class="p-top-news__category"><?php echo esc_html($categories[0]->name); ?></span>
+                                <?php endif; ?>
+                                <span class="p-top-news__item-title"><?php the_title(); ?></span>
+                            </a>
+                        </li>
+                        <?php
                     endwhile;
                     wp_reset_postdata();
-                else :
-                ?>
-                <li class="p-top-news__item">
-                    <p class="p-top-news__no-posts">現在、お知らせはありません。</p>
-                </li>
+                else:
+                    ?>
+                    <li class="p-top-news__item">
+                        <p class="p-top-news__no-posts">現在、お知らせはありません。</p>
+                    </li>
                 <?php endif; ?>
             </ul>
             <div class="p-top-news__more">
-                <a href="<?php echo esc_url(home_url('/news')); ?>" class="c-btn c-btn--secondary c-btn--arrow">すべて見る</a>
+                <a href="<?php echo esc_url(home_url('/news')); ?>"
+                    class="c-btn c-btn--secondary c-btn--arrow">すべて見る</a>
             </div>
         </div>
     </section>
@@ -75,7 +84,8 @@ get_header();
                 <div class="p-top-features__item">
                     <div class="p-top-features__icon">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/>
+                            <path
+                                d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z" />
                         </svg>
                     </div>
                     <h3 class="p-top-features__item-title">救急体制</h3>
@@ -84,7 +94,8 @@ get_header();
                 <div class="p-top-features__item">
                     <div class="p-top-features__icon">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                            <path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-7 3c1.93 0 3.5 1.57 3.5 3.5S13.93 13 12 13s-3.5-1.57-3.5-3.5S10.07 6 12 6zm7 13H5v-.23c0-.62.28-1.2.76-1.58C7.47 15.82 9.64 15 12 15s4.53.82 6.24 2.19c.48.38.76.97.76 1.58V19z"/>
+                            <path
+                                d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-7 3c1.93 0 3.5 1.57 3.5 3.5S13.93 13 12 13s-3.5-1.57-3.5-3.5S10.07 6 12 6zm7 13H5v-.23c0-.62.28-1.2.76-1.58C7.47 15.82 9.64 15 12 15s4.53.82 6.24 2.19c.48.38.76.97.76 1.58V19z" />
                         </svg>
                     </div>
                     <h3 class="p-top-features__item-title">入院対応</h3>
@@ -93,7 +104,8 @@ get_header();
                 <div class="p-top-features__item">
                     <div class="p-top-features__icon">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                            <path d="M20 6h-4V4c0-1.1-.9-2-2-2h-4c-1.1 0-2 .9-2 2v2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm-8-2h4v2h-4V4zM4 20V8h16v12H4zm8-10c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"/>
+                            <path
+                                d="M20 6h-4V4c0-1.1-.9-2-2-2h-4c-1.1 0-2 .9-2 2v2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm-8-2h4v2h-4V4zM4 20V8h16v12H4zm8-10c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z" />
                         </svg>
                     </div>
                     <h3 class="p-top-features__item-title">院内設備</h3>
@@ -101,7 +113,8 @@ get_header();
                 </div>
             </div>
             <div class="p-top-features__more">
-                <a href="<?php echo esc_url(home_url('/features')); ?>" class="c-btn c-btn--secondary c-btn--arrow">もっと見る</a>
+                <a href="<?php echo esc_url(home_url('/features')); ?>"
+                    class="c-btn c-btn--secondary c-btn--arrow">もっと見る</a>
             </div>
         </div>
     </section>
@@ -112,29 +125,37 @@ get_header();
             <h2 class="p-top-departments__title">診療科目</h2>
             <ul class="p-top-departments__list">
                 <li class="p-top-departments__item">
-                    <a href="<?php echo esc_url(home_url('/departments/internal')); ?>" class="p-top-departments__link">内科</a>
+                    <a href="<?php echo esc_url(home_url('/departments/internal')); ?>"
+                        class="p-top-departments__link">内科</a>
                 </li>
                 <li class="p-top-departments__item">
-                    <a href="<?php echo esc_url(home_url('/departments/surgery')); ?>" class="p-top-departments__link">外科</a>
+                    <a href="<?php echo esc_url(home_url('/departments/surgery')); ?>"
+                        class="p-top-departments__link">外科</a>
                 </li>
                 <li class="p-top-departments__item">
-                    <a href="<?php echo esc_url(home_url('/departments/proctology')); ?>" class="p-top-departments__link">肛門外科</a>
+                    <a href="<?php echo esc_url(home_url('/departments/proctology')); ?>"
+                        class="p-top-departments__link">肛門外科</a>
                 </li>
                 <li class="p-top-departments__item">
-                    <a href="<?php echo esc_url(home_url('/departments/orthopedics')); ?>" class="p-top-departments__link">整形外科</a>
+                    <a href="<?php echo esc_url(home_url('/departments/orthopedics')); ?>"
+                        class="p-top-departments__link">整形外科</a>
                 </li>
                 <li class="p-top-departments__item">
-                    <a href="<?php echo esc_url(home_url('/departments/urology')); ?>" class="p-top-departments__link">泌尿器科</a>
+                    <a href="<?php echo esc_url(home_url('/departments/urology')); ?>"
+                        class="p-top-departments__link">泌尿器科</a>
                 </li>
                 <li class="p-top-departments__item">
-                    <a href="<?php echo esc_url(home_url('/departments/dermatology')); ?>" class="p-top-departments__link">皮膚科</a>
+                    <a href="<?php echo esc_url(home_url('/departments/dermatology')); ?>"
+                        class="p-top-departments__link">皮膚科</a>
                 </li>
                 <li class="p-top-departments__item">
-                    <a href="<?php echo esc_url(home_url('/departments/rehabilitation')); ?>" class="p-top-departments__link">リハビリテーション科</a>
+                    <a href="<?php echo esc_url(home_url('/departments/rehabilitation')); ?>"
+                        class="p-top-departments__link">リハビリテーション科</a>
                 </li>
             </ul>
             <div class="p-top-departments__more">
-                <a href="<?php echo esc_url(home_url('/departments')); ?>" class="c-btn c-btn--secondary c-btn--arrow">診療案内を見る</a>
+                <a href="<?php echo esc_url(home_url('/departments')); ?>"
+                    class="c-btn c-btn--secondary c-btn--arrow">診療案内を見る</a>
             </div>
         </div>
     </section>
@@ -211,11 +232,7 @@ get_header();
                 <div class="p-top-access__map">
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3240.8280303808788!2d139.76493!3d35.6812362!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzXCsDQwJzUyLjQiTiAxMznCsDQ1JzUzLjgiRQ!5e0!3m2!1sja!2sjp!4v1234567890123"
-                        width="100%"
-                        height="300"
-                        style="border:0;"
-                        allowfullscreen=""
-                        loading="lazy"
+                        width="100%" height="300" style="border:0;" allowfullscreen="" loading="lazy"
                         referrerpolicy="no-referrer-when-downgrade">
                     </iframe>
                 </div>
@@ -230,7 +247,8 @@ get_header();
             <p class="p-top-cta__text">お気軽にお電話ください。<br class="u-sp-only">スタッフが丁寧に対応いたします。</p>
             <div class="p-top-cta__buttons">
                 <a href="tel:0120-000-000" class="c-btn c-btn--phone c-btn--lg">0120-000-000</a>
-                <a href="<?php echo esc_url(home_url('/contact')); ?>" class="c-btn c-btn--secondary c-btn--lg">お問い合わせフォーム</a>
+                <a href="<?php echo esc_url(home_url('/contact')); ?>"
+                    class="c-btn c-btn--secondary c-btn--lg">お問い合わせフォーム</a>
             </div>
         </div>
     </section>
