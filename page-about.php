29398e7c6024515e77afe7ea2db3a60666583788
ryotohiji <ryotohiji.41@gmail.com>
2026-08-21 21:50:17 +0900

    新谷医師を削除

--- page-about.php
+++ page-about.php
@@ -100,30 +100,6 @@ get_header(); ?>
                         </div>
                     </div>
                 </div>
-                <!-- 常勤医師 -->
-                <div class="p-about-doctor__card p-about-doctor__card--sub">
-                    <div class="p-about-doctor__icon-area">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="40"
-                            height="40">
-                            <path
-                                d="M12 12c2.7 0 4.8-2.1 4.8-4.8S14.7 2.4 12 2.4 7.2 4.5 7.2 7.2 9.3 12 12 12zm0 2.4c-3.2 0-9.6 1.6-9.6 4.8v2.4h19.2v-2.4c0-3.2-6.4-4.8-9.6-4.8z" />
-                        </svg>
-                    </div>
-                    <div class="p-about-doctor__info">
-                        <div class="p-about-doctor__header">
-                            <h3 class="p-about-doctor__name">新谷 慎太郎</h3>
-                            <span class="p-about-doctor__role">常勤医師</span>
-                        </div>
-                        <p class="p-about-doctor__specialty">外科、内科、リハビリテーション科</p>
-                        <div class="p-about-doctor__qualifications">
-                            <span class="p-about-doctor__qualifications-label">資格</span>
-                            <ul>
-                                <li>医学博士</li>
-                                <li>日本外科学会認定医</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
                 <!-- 非常勤医師1 -->
                 <div class="p-about-doctor__card p-about-doctor__card--sub">
                     <div class="p-about-doctor__icon-area">
